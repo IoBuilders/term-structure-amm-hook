247a54ea727be82ea52a1cb9e422f11417311ea0
rdf5 <robertodf99@gmail.com>
2025-06-24 18:33:04 +0200

    fix: cli
    Signed-off-by: rdf5 <robertodf99@gmail.com>

--- cli/src/blockchain/ERC20/command/NewERC20Command.ts
+++ cli/src/blockchain/ERC20/command/NewERC20Command.ts
@@ -1,20 +1,20 @@
-import { ERC20Artifact } from 'blockchain/artifact';
-import { NewBaseContractCommand } from '@blockchain/base-contract';
-import type { AtsControlList } from '@blockchain/AtsControlList';
+import { ERC20Artifact } from "blockchain/artifact";
+import { NewBaseContractCommand } from "@blockchain/base-contract";
+import type { AtsControlList } from "@blockchain/AtsControlList";
 
 const { abi: ABI } = ERC20Artifact;
 
 interface NewERC20CommandParams
-    extends Omit<NewBaseContractCommand<typeof ABI>, 'abi' | 'eventList'> {
-    atsControlList: AtsControlList;
+  extends Omit<NewBaseContractCommand<typeof ABI>, "abi" | "eventList"> {
+  atsControlList: AtsControlList;
 }
 
 export default class NewERC20Command extends NewBaseContractCommand<
-    typeof ABI
+  typeof ABI
 > {
-    public atsControlList: AtsControlList;
-    constructor({ address, account, atsControlList }: NewERC20CommandParams) {
-        super({ abi: ABI, address, account, eventList: [] });
-        this.atsControlList = atsControlList;
-    }
+  public atsControlList: AtsControlList;
+  constructor({ address, account, atsControlList }: NewERC20CommandParams) {
+    super({ abi: ABI, address, account, eventList: [] });
+    this.atsControlList = atsControlList;
+  }
 }
